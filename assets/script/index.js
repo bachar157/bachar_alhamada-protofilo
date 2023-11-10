2ef6ac8a9aa328093461819d8de33999af8c37c3
bachar-alhamada <bachar.alhamada76@gmail.com>
2023-11-10 12:36:14 -0600

    finsh the first part of CSS fill

---
+++ assets/script/index.js
@@ -0,0 +1,45 @@
+'use strit'
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+function onEvent(event, selector,callback){
+    return selector.addEventListener(event, callback)
+}
+$(window).scroll(function(){
+    if($(this).scrollTop()>=200){
+        $("#navBar").addClass("noTransparrent");
+     }
+    else{
+        $("#navBar").removeClass("noTransparrent");
+     }
+});
+
+
+
+$(document).ready(function () {
+    $('a.scroll').on('click', function (event) {
+      var hash = this.hash
+  
+      $('html, body').animate(
+        { scrollTop: $(hash).offset().top - 90 },
+        800,
+        function () {}
+      )
+    });
+  
+    //if (checkVisible($('#statistics'))) {
+      $('.circle')
+        .circleProgress({
+          startAngle: -Math.PI / 2,
+          fill: '#0575e6'
+        })
+        .on('circle-animation-progress', function (event, progress, stepValue) {
+          $(this)
+            .find('span')
+            .html(Math.round(stepValue * 100) + '%')
+        })
+   // }
+  });
+$(function () {
+    $("#emailForm").validate();
+ });
